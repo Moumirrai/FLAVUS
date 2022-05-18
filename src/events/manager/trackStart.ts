bf5ee04a31bb569e47cbb27c0f71b973bd4f50ac
Moumirrai <72272440+Moumirrai@users.noreply.github.com>
2022-05-18 20:11:54 +0200

    pause on end fix

--- src/events/manager/trackStart.ts
+++ src/events/manager/trackStart.ts
@@ -5,6 +5,11 @@ const trackStartEvent: iManagerEvent = {
   name: 'trackStart',
   async execute(client, _manager, player: Player) {
     player.set(`previousTrack`, player.queue.current);
+    if (player.get('pauseOnEnd')) {
+      player.set('pauseOnEnd', false);
+      if (!player || !player.playing) return
+      player.pause(true);
+    }
   }
 };
 
