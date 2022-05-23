dfce1f0ba2c1026082d55be52e12ab55b29da644
Moumirrai <72272440+Moumirrai@users.noreply.github.com>
2022-05-23 22:44:01 +0200

    blacklist cmd

---
+++ src/commands/blacklist.ts
@@ -0,0 +1,26 @@
+import { Message, MessageEmbed } from 'discord.js';
+import { CommandArgs, iCommand } from 'my-module';
+
+const BlacklistCommand: iCommand = {
+  name: 'blacklist',
+  aliases: ["bl"],
+  voiceRequired: false,
+  joinPermissionRequired: false,
+  playerRequired: false,
+  sameChannelRequired: false,
+  visible: true,
+  description: 'Sends a dashboard url',
+  usage: 'ping',
+  async execute({ client, message }: CommandArgs): Promise<Message> {
+    return message.channel.send({
+      embeds: [
+        new MessageEmbed()
+          .setColor(client.config.embed.color)
+          .setTitle('Blacklist - Dashboard')
+          .setURL("http://130.61.29.155:8080/")
+      ]
+    });
+  }
+};
+
+export default BlacklistCommand;
