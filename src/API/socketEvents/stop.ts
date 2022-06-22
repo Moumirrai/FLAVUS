57da2d2404b023b5770d6ec0a768b0e64cb4f916
Moumirrai <72272440+Moumirrai@users.noreply.github.com>
2022-06-22 23:12:41 +0200

    stop event

---
+++ src/API/socketEvents/stop.ts
@@ -0,0 +1,21 @@
+import { SocketEvent } from 'flavus-api';
+import type { Player } from 'erela.js';
+import { getPlayer } from '../player';
+
+//TODO: fix or delete
+
+const StopEvent: SocketEvent = {
+  name: 'stop',
+  async execute(client, socket, data: boolean): Promise<any> {
+    const voiceCache = client.voiceCache.get(socket.request.session.user.id)
+    if (!voiceCache) return socket.emit('playerError', 'I can\'t see you connected!')
+    const player: Player = client.manager.players.get(
+      voiceCache.voiceChannel.guild.id
+    )
+    if (!player) return socket.emit('playerError', 'Nothing to stop!')
+    player.destroy()
+    getPlayer(client, socket)
+  }
+};
+
+export default StopEvent;
